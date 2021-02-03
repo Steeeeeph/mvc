364bbe4b791bacb29124d816f2570030641cbd46
Manten Devillé <manten.d@hotmail.com>
2021-02-03 12:41:08 +0100

    Finish navbar, bug: collapse button not working yet

--- View/signe.php
+++ View/signe.php
@@ -6,6 +6,7 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://use.fontawesome.com/f92a08f756.js"></script>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -19,21 +20,41 @@
 <body>
 
     <!-- Navigation -->
-    <nav class="navbar navbard-expand-md navbar-light navcolor sticky-top">
+    <nav class="navbar navbar-expand-md navbar-light navcolor sticky-top">
         <div class="container-fluid">
             <a href="#" class="navbar-brand logolink">
                 <img src="../assets/book.png" class="navlogo ">
                 <p class="classlogoname">Bookie</p>
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+
+
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a href="#" class="nav-link">Bookieshelf</a>
+                    </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link"></a></li>
+                        <a href="#" class="nav-link">User</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Home</a>
+                    </li>
                 </ul>
+
+
+
             </div>
+
+            <div class="userSettings collapse navbar-collapse container-fluid" id="userSettings">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#" class="nav-link"><span class="fa fa-user"></span> Sign Up</a></li>
+                    <li><a href="#" class="nav-link"><span class="fa fa-sign-in"></span> Login</a></li>
+                </ul>
+                
+            </div>
+            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#userSettings">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
         </div>
     </nav>
     <!-- End of Navigation -->
@@ -41,6 +62,8 @@
 
     <header>
         <h1>Welcome to my site</h1>
+        <span class="glyphicon glyphicon-user"></span>
+
     </header>
     <form method="post">
         <h2>Register Form</h2>
@@ -81,9 +104,9 @@
     <footer>
         &copy; BeCode <?php echo date('Y')?>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
